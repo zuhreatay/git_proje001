46f1fd39b8645abd5aefd4c4dd804fd7c5eeeb42
zuhreatay <atayzuhre61@gmail.com>
2020-06-21 16:10:48 -0500

    new changes

--- src/git_proje001/FirstClass.java
+++ src/git_proje001/FirstClass.java
@@ -3,8 +3,8 @@ package git_proje001;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("ilk commit from local repository");
+        System.out.println("new changes");
 	}
 
 }
